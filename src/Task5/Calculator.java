68346fea0b28af3819358f088b1deca71c09f1e2
LianaBo <liana.bobrikova@bk.ru>
2021-09-22 22:13:42 +0300

    Task5 was added

---
+++ src/Task5/Calculator.java
@@ -0,0 +1,30 @@
+package Task5;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        String arithmeticOper = scanner.next();
+
+        switch (arithmeticOper) {
+            case "/":
+                if (b != 0) System.out.println(a / b);
+                else System.out.println("На 0 делить нельзя!");
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            default:
+                System.out.println("Некоректный оператор");
+        }
+    }
+}
