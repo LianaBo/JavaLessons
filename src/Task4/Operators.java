1a934289da4401d02e9306aae73778c64580f715
LianaBo <liana.bobrikova@bk.ru>
2021-09-23 13:23:50 +0300

    code refactoring

--- src/Task4/Operators.java
+++ src/Task4/Operators.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class Operators {
     public static void main(String[] args) {
+        MathOperation mathOperation = new MathOperation();
         Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
         double y = scanner.nextDouble();
         double z = scanner.nextDouble();
-        double sumValue = (x + y + z) / 3;
-        int roundingValue = (int) sumValue / 2;
 
-        System.out.println(sumValue);
-        System.out.println((roundingValue > 3) ? "Программа выполнена корректно" : "Error");
+        double averageOfThreeNumbers = mathOperation.arithmeticAverage(x, y, z);
+        System.out.println("Среднее арифметическое трех чисел: " + averageOfThreeNumbers);
+
+        int roundingAverage = (int) averageOfThreeNumbers;
+        System.out.println((roundingAverage / 2 > 3) ? "Программа выполнена корректно" : "Error");
     }
+}
+
+class MathOperation {
+    public double sumThreeNumbers(double x, double y, double z) {
+        return x + y + z;
+    }
+
+    public double arithmeticAverage(double x, double y, double z) {
+        return sumThreeNumbers(x, y, z) / 3;
+    }
+
 }
