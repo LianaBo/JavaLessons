dd21afa18b55d147794303b73e4e3ad5800723ad
LianaBo <liana.bobrikova@bk.ru>
2021-09-26 18:54:37 +0300

    fix in rounding

--- src/Task4/Operators.java
+++ src/Task4/Operators.java
@@ -3,28 +3,27 @@ package Task4;
 import java.util.Scanner;
 
 public class Operators {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        MathOperation mathOperation = new MathOperation();
-        Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
         double y = scanner.nextDouble();
         double z = scanner.nextDouble();
 
-        double averageOfThreeNumbers = mathOperation.arithmeticAverage(x, y, z);
-        System.out.println("Среднее арифметическое трех чисел: " + averageOfThreeNumbers);
+        System.out.println("Среднее арифметическое трех чисел: " + avgNumbers(x, y, z));
 
-        int roundingAverage = (int) averageOfThreeNumbers;
-        System.out.println((roundingAverage / 2 > 3) ? "Программа выполнена корректно" : "Error");
+        double roundingAvg = Math.floor(Operators.avgNumbers(x, y, z) / 2);
+        System.out.println(roundingAvg);
+        System.out.println((roundingAvg > 3) ? "Программа выполнена корректно" : "Error");
     }
-}
 
-class MathOperation {
-    public double sumThreeNumbers(double x, double y, double z) {
+    public static double sumThreeNumbers(double x, double y, double z) {
         return x + y + z;
     }
 
-    public double arithmeticAverage(double x, double y, double z) {
+    public static double avgNumbers(double x, double y, double z) {
         return sumThreeNumbers(x, y, z) / 3;
     }
+
 
 }
