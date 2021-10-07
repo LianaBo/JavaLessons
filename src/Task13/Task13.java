7b14521f879da7b3d2225be9e076c512dca19136
LianaBo <liana.bobrikova@bk.ru>
2021-10-07 22:11:35 +0300

    Add Task13

---
+++ src/Task13/Task13.java
@@ -0,0 +1,30 @@
+package Task13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task13 {
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        String string = scanner.nextLine();
+        checkString(string);
+    }
+
+    public static void checkString(String string) {
+        String[] arrString = string.split(" "); // разделяем строку
+        System.out.println(Arrays.deepToString(arrString));
+        int count = 0;
+        for (int i = 0; i < arrString.length; i++) {
+            if (arrString[i].matches("[^а-яА-Я0-9]*")){  // регулярное выражение от Аа-Яя и 0 - 9
+                System.out.println(arrString[i]);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
