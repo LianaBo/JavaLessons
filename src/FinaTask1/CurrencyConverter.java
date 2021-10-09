4d3c65eee0a7f6c9f25f24322f338490bbb70fd0
LianaBo <liana.bobrikova@bk.ru>
2021-10-09 15:23:49 +0300

    Add FinaTask1

---
+++ src/FinaTask1/CurrencyConverter.java
@@ -0,0 +1,19 @@
+package FinaTask1;
+
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Введите текущий курс доллара: ");
+        double currUSD = in.nextDouble();
+        System.out.println("Введите количество рублей: ");
+        double currRUB = in.nextDouble();
+        printConvertRubOfUsd(currUSD, currRUB);
+    }
+
+    public static void printConvertRubOfUsd(double currUSD, double currRUB) {
+        System.out.println("Итого: " + String.format("%.2f", currRUB / currUSD) + " долларов");
+    }
+}
