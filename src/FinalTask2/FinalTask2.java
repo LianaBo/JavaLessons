e932a67f9603eb6a2eda22baa2ba22fc8a4bbd70
LianaBo <liana.bobrikova@bk.ru>
2021-10-14 00:58:23 +0300

    Refactoring code

--- src/FinalTask2/FinalTask2.java
+++ src/FinalTask2/FinalTask2.java
@@ -8,60 +8,41 @@ public class FinalTask2 {
 
     public static void main(String[] args) {
         System.out.println("Введите уравнение с одной неизвестной: ");
-        String equation = in.next(); // вводим строку и разбиваем ее на элементы массива
-        char[] equationArr = equation.toCharArray();
+        String equation = in.next();
+        String[] equationArr = equation.split("");// разбиваем строку на элементы массива
         if (equationArr.length == 5) {
-            int positionX = equation.indexOf('x');
-            System.out.println(Arrays.toString(equationArr));
-            checkArr(positionX, equationArr);
+            checkArr(equationArr);
         } else {
             System.out.println("Попробуйте еще раз!");
         }
     }
 
-    public static void checkArr(int positionX, char[] equationArr) {
-        switch (positionX) {
-            case 0:
-                firstPosition(equationArr);
-                break;
-            case 2:
-                middlePosition(equationArr);
-                break;
-            case 4:
-                lastPosition(equationArr);
-                break;
-        }
-    }
+    public static void checkArr(String[] equationArr) {
+        int sumBeforeEquality = 0;
+        int globalСoefficient = 1;
+        int localСoefficient = 1;
+        int coefficientX = 1;
+        int sum = 0;
+        int i = 0;
 
-    public static void firstPosition(char[] equationArr) {
-        int middleInt = Character.digit(equationArr[2], 10);
-        int lastInt = Character.digit(equationArr[4], 10);
-        if (equationArr[1] == '+') {
-            System.out.println("Вывод: " + (lastInt - middleInt));
-        } else if (equationArr[1] == '-') {
-            System.out.println("Вывод: " + (lastInt + middleInt));
+        while (i < equationArr.length) {
+            if (equationArr[i].equals("+") || equationArr[i].equals("-")) {
+                if (equationArr[i].equals("-")) {
+                    localСoefficient = -1;
+                }
+            } else if (equationArr[i].equals("x")) {
+                coefficientX = localСoefficient;
+                globalСoefficient = -1;
+            } else if (equationArr[i].equals("=")) {
+                localСoefficient = 1;
+                sumBeforeEquality = sum * globalСoefficient;
+                sum = 0;
+            } else {
+                sum += Integer.valueOf(equationArr[i]) * localСoefficient;
+            }
+            i++;
         }
-    }
-
-    public static void middlePosition(char[] equationArr) {
-        int firstInt = Character.digit(equationArr[0], 10);
-        int lastInt = Character.digit(equationArr[4], 10);
-        if (equationArr[1] == '+') {
-            System.out.println("Вывод: " + (firstInt - lastInt));
-        } else if (equationArr[1] == '-') {
-            System.out.println("Вывод: " + (firstInt - lastInt));
-        }
-    }
-
-    public static void lastPosition(char[] equationArr) {
-        int firstInt = Character.digit(equationArr[0], 10);
-        int middleInt = Character.digit(equationArr[2], 10);
-
-        if (equationArr[1] == '+') {
-            System.out.println("Вывод: " + (firstInt + middleInt));
-        } else if (equationArr[1] == '-') {
-            System.out.println("Вывод: " + (firstInt - middleInt));
-        }
+        System.out.println(coefficientX * (sum + sumBeforeEquality));
     }
 }
 
